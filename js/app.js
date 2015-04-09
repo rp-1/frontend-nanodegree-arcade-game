7c398a6d77824c511488b761129238065bb802a0
rp-1 <richpaschall@comcast.net>
2015-04-09 13:39:55 -0500

    Add player object basic setup

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 200;
+    this.y = 200;
 }
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 100;
+    this.y = 100;
+}
+
+Player.prototype.update = function(dt) {
+    
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(args) {
+    console.log("in Player.handleInput " + args);
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var testEnemy1 = new Enemy();
+var testEnemy2 = new Enemy();
+var allEnemies = [testEnemy1, testEnemy2];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -41,6 +64,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+    
     player.handleInput(allowedKeys[e.keyCode]);
 });
